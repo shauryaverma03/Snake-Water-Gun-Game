cba171287f961009a7a8103fcf7b1e9787bec5e9
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2025-01-22 00:35:23 +0530

    Create main_short.py

---
+++ main_short.py
@@ -0,0 +1,40 @@
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+import random
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "Snake", -1 : "Water", 0 : "Gun"}
+you = youDict[youstr]
+
+print(f"you chose {reverseDict[you]} \nComputer chose {reverseDict[computer]}")
+
+if (computer == you) :
+    print("It's a draw")
+else :
+    '''
+    if (computer == -1 and you == 1): (computer - you) = -2
+        print("you win")
+    elif (computer == -1 and you == 0): (computer - you) = -1
+        print("You Lose")
+    elif (computer == 1 and you == -1): (computer - you) = 2
+        print("you lose")
+    elif (computer == 1 and you == 0): (computer - you) = 1
+        print("You Win")
+    elif (computer == 0 and you == -1): (computer - you) = 1
+        print("you win")
+    elif (computer == 0 and you == 1): (computer - you) = -1
+        print("You Lose")
+    else :
+        print("Something went wrong!")
+
+        The below logic is written on the basic of the value of computer - you
+    '''
+    if ((computer - you) == -1 or (computer - you) == 2) :
+        print("you lose")
+    else :
+        print("you win")
